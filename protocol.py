06836051cdf595e7bbd8d6dbe14882b2c4d4033d
Jackson Xie <jacksonxie@Jacksons-Air.hitronhub.home>
2021-10-10 15:40:46 -0700

    pair program with vick

--- protocol.py
+++ protocol.py
@@ -33,12 +33,14 @@ class Protocol:
         self.shared_key = sharedKey
         self.name = name
         self.state = ProtocolState.UNKNOWN
+        self.sent_nonce = None
 
 
     # Creating the initial message of your protocol (to be send to the other party to bootstrap the protocol)
     # TODO: IMPLEMENT THE LOGIC (MODIFY THE INPUT ARGUMENTS AS YOU SEEM FIT)
     def GetProtocolInitiationMessage(self):
         nonce = self._makeNewNonce()
+        self.sent_nonce = nonce
         payload = {"nonce": nonce, "name": self.name, "type": MsgType.INIT}
         return json.dumps(payload)
     
@@ -53,26 +55,41 @@ class Protocol:
     def IsMessagePartOfProtocol(self, message):
         payload = {}
         type = payload["type"]
-        return self._isExpectedMsgType(type) and self._isValidMsgFormat(type, payload)
-
+        return self._isProtocolType(type) 
+    
+    def _isProtocolType(self, type):
+    	return type in set(MsgType.INIT, MsgType.INIT_REPLY, MsgType.END)
     
     """
     Message formats in JSON 
-    INIT: {name: "client"|"server", nonce: int}
+    INIT: {name: "client"|"server", nonce: int, type: MsgType}
     INIT_REPLY: {nonce: int, encrypted: E({name: "client"|"server", dh: int, nonce: int}, K_ab)}
     END: {encrypted: E({name: "client"|"server", dh: int, nonce: int}, K_ab)}
+    REGULAR: {ecrypted: E(str)}
     """
     def _isValidMsgFormat(self, type, payload):
         validations = []
         if type == MsgType.INIT:
             is_valid_name = payload["name"] == "client" or payload["name"] == "server"
             is_valid_nonce = payload["nonce"].isnumeric()
+            
             validations.extend([is_valid_name, is_valid_nonce])
         elif type == MsgType.INIT_REPLY:
             is_valid_nonce = payload["nonce"].isnumeric()
-            reply_nonce = payload["reply_nonce"]
-            is_valid_name = payload["name"] == "client" or payload["name"] == "server"
-            is_valid_reply_nonce = reply_nonce.isnumeric() and int(reply_nonce) == self.sent_nonce
+            encrypted = payload["encrypted"]
+            is_valid_reply_name = encrypted["name"] == "client" or encrypted["name"] == "server"
+            is_valid_reply_nonce = encrypted["nonce"].isnumeric() and int(encrypted["nonce"]) == self.sent_nonce
+            is_valid_dh = encrypted["dh"].isnumeric()
+            
+            validations.extend([is_valid_nonce, is_valid_reply_name, is_valid_reply_name, is_valid_dh])
+        elif type == MsgType.END:
+            encrypted = payload["encrypted"]
+            is_valid_reply_name = encrypted["name"] == "client" or encrypted["name"] == "server"
+            is_valid_reply_nonce = encrypted["nonce"].isnumeric() and int(encrypted["nonce"]) == self.sent_nonce
+          	
+            validations.extend([is_valid_reply_name, is_valid_reply_nonce])
+		#TODO: continue one guys!
+        return all(validations)
 
             
 
@@ -81,12 +98,31 @@ class Protocol:
         pass
     
     def _getNextState(self):
-        pass
+        if self.state == ProtocolState.UNKNOWN:
+            # TODO dont hardcode these strings
+            if self.name == "client":
+                return ProtocolState.RECEIVED_INIT
+            elif self.name == "server":
+                return ProtocolState.SENT_INIT
+            else:
+                raise Exception("Invalid name")
+ 
+        if self.state == ProtocolState.SENT_INIT:
+            return ProtocolState.RECEIVED_INIT_REPLY
+        elif self.state == ProtocolState.RECEIVED_INIT_REPLY:
+            return ProtocolState.END
+        elif self.state == ProtocolState.RECEIVED_INIT:
+            return ProtocolState.SENT_INIT_REPLY
+        elif self.state == ProtocolState.SENT_INIT_REPLY:
+            return ProtocolState.END
+        else:
+            raise Exception("Invalid current protocol state")
 
     # Processing protocol message
     # TODO: IMPLMENET THE LOGIC (CALL SetSessionKey ONCE YOU HAVE THE KEY ESTABLISHED)
     # THROW EXCEPTION IF AUTHENTICATION FAILS
     def ProcessReceivedProtocolMessage(self, message):
+      	#self._isValidMsgFormat(type, payload)
         pass
 
 
